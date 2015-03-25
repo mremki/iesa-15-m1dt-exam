867c1f7168c3d61e59079c6aacd0e3654d78d2f0
mremki <billel_remki@live.fr>
2015-03-25 11:38:09 +0100

    Décrypteur avec un test unitaire

--- enigma/enigma.js
+++ enigma/enigma.js
@@ -1,5 +1,26 @@
 /* Classe Enigma */
 function Enigma() 
 { 	
-
+	var textCrypter = "jdwovarig xunsyqcp'fmztkhbel.,:;";
+	var texteDecrypter =   "abcdefghijklmnopqrstuvwxyz '.,:;";
+	
+	this.encrypt = function (text) {
+		textResult = "";
+		var j;
+		for (var i=0; i < text.length; i++){
+			j = texteDecrypter.indexOf(text[i]);
+			textResult += textCrypter.charAt(j);
+		}
+		
+		return textResult;
+	}
+	this.decrypt = function (text) {
+		textResult = "";
+		var j;
+		for (var i=0; i < text.length; i++){
+			j = textCrypter.indexOf(text[i]);
+			textResult += texteDecrypter.charAt(j);
+		}
+		return textResult;
+	}
 }
