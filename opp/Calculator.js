08db4fa71d137239a8f936a3d388cdccf9897b48
mremki <billel_remki@live.fr>
2015-03-25 14:23:18 +0100

    All Behavior for Calculator

--- opp/Calculator.js
+++ opp/Calculator.js
@@ -3,19 +3,33 @@ function Calculator()
 { 	
 	this.result = 0;
 	
+	
 	this.add = function(x) {
-		this.result += parseFloat(x);
+		if( this.add.arguments.length == 1 &&
+		  !isNaN(x) ){
+			this.result += x;
+		}
 	}
 
 	this.minus = function(x) {
-		this.result -= parseFloat(x);
-	}
-
-	this.divide  = function(x) {
-		this.result *= parseFloat(x);
+		if( this.minus.arguments.length == 1 &&
+		  !isNaN(x) ){
+			this.result -= x;
+		}
 	}
 
 	this.multiply = function(x) {
-		this.result /= parseFloat(x);
+		if( this.multiply.arguments.length == 1 &&
+		  !isNaN(x) ){
+			this.result *= parseFloat(x);
+		}
 	}
+	
+	this.divide  = function(x) {
+		if( this.divide.arguments.length == 1 &&
+		  !isNaN(x) && x != 0){
+			this.result = this.result / x;
+		}
+	}
+
 }
